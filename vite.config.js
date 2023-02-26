dff1628fb834ba7dd64519c61f984e8b33e827df
Reece Williams <reecepbcups@gmail.com>
2023-02-25 23:27:26 -0600

    dockerize it for safty because of JS & CSS Injection

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ const config = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	},
+	server: {
+        host: true,
+        port: 8080
+    }
 };
 
 export default config;
