8fc729aa08ee33a74ea840e868a6edb7997d60af
Reece Williams <Reecepbcups@gmail.com>
2023-02-25 23:38:46 -0600

    detach mode

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 VERSION=0.0.1
 
 docker build -t reecepbcups/justjoe-app:$VERSION .
-docker run --rm --name=justjoe-app -p 6001:6001 reecepbcups/justjoe-app:$VERSION
+docker run --detach --rm --name=justjoe-app -p 6001:6001 reecepbcups/justjoe-app:$VERSION
